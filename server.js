50c4c639c329d5a5fbf4afd8c1c99d84e3a7b2e1
Stanley Lim <stanley.lim@cyndx.com>
2022-02-05 14:01:03 -0500

    add some code

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+const users = [
+  { id: 0, name: 'stanley', coolness: 0 },
+  { id: 1, name: 'eric', coolness: 100 },
+  { id: 2, name: 'jonathan', coolness: 100 },
+  { id: 3, name: 'kenneth', coolness: 2 },
+];
+
+app.get('/', (req, res, next) => {
+  res.send(`
+    <html>
+      <body>
+        <ul>
+          ${users.map(({ id, name }) => `<a href='/${id}'>${name}</a>`).join('')}
+        </ul>
+      </body>
+    </html>
+  `);
+});
+
+app.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+  const user = users.find(user => user.id === +id);
+  res.send(`
+    <html>
+      <body>
+        ${user.name}
+        ${user.coolness}
+      </body>
+    </html>
+  `);
+});
+
+app.listen(3000)
